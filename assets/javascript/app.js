baaef254309df9ec41c1a9de18f7d561364f293c
Ronnie Larson <ronnielarson17@gmail.com>
2019-06-02 16:30:31 -0600

    Made variables in the js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+// These variables will hold the questions.
+var questions = ["How many days are in a year", "How old is earth", "How much is 100 euros in US currency", "How many pennies are in a million dollars"];
+
+// This is where the randomly generated questions will go 
+var chosenQuestion = "";
+
+// these variables will hold the multiple choice anwsers for the questions
+var anwsers1 = "";
+var anwsers2 = "";
+var anwsers3 = "";
+var anwsers4 = "";
+
+// This variable will hold the anwser you've chosen.
+var chosenAnwser = "";
+
+// This variable will hold the number of wrong guesses
+var wrongGuessCounter = 0;
+
+//This variable will hold the number of right guesses
+var rightGuessCounter = 0;
+
+// This is the timer variable
+var timer = 30;
+
+
+
+
